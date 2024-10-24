0b5b20c237dde12c0eed5962f5cc661a6212e5c1
Qasem <Qasem.najim@gmail.com>
2024-10-24 13:13:35 -0700

    what i write ?

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <div className="mt-[190px]" >
+        <div className="mt-[100px]" >
           <About />
           <Experience />
         </div>
