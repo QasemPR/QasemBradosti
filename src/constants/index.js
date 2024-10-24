957002b787ea7bdc7f8e62ad91d90232ba7771f6
Qasem <Qasem.najim@gmail.com>
2024-10-24 15:56:22 -0700

    Completed!

--- src/constants/index.js
+++ src/constants/index.js
@@ -43,11 +43,11 @@ export const navLinks = [
   {
     id: "skills",
     title: "Skills",
-  }
+  },
 ];
 
 const services = [
-    {
+  {
     title: "Full  Stack Developer",
     icon: creator,
   },
@@ -140,8 +140,7 @@ const experiences = [
   {
     title: "Full Stack Developer",
     company_name: "Techno Speed Software Team",
-    icon: starbucks,
-    iconBg: "#383E56",
+    iconBg: "#0070ff",
     date: "March 2024 - Now",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
@@ -151,23 +150,42 @@ const experiences = [
     ],
   },
   {
-    title: "IT Supporter",
-    company_name: "Sardam",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2019 - Feb 2021",
+    title: "IT Support & Technical Support Specialist",
+    company_name: "Bradost PC and Mobile Center",
+    iconBg: "#0070ff",
+    date: "Jan 2018 - Jul 2019",
     points: [
-      "I have experience in technology and troubleshooting mobile hardware and software issues , and I worked in my mobile store (Sardam For Mobile) in Sidakan - Soran.",
+      "Over the past two years, I have gained extensive experience in providing IT support and technical assistance to users, ensuring smooth and efficient operation of their systems and devices. My role involved",
+      "Troubleshooting and Issue Resolution: Diagnosed and resolved hardware, software, and network-related issues, minimizing downtime and enhancing productivity.",
+      "System Maintenance and Upgrades: Performed regular system checks, software updates, and hardware maintenance to keep IT infrastructure running smoothly.",
+      "Technical Assistance: Provided remote and in-person support for end-users, guiding them through the use of software, devices, and other technical tools.",
+      "Setup and Configuration: Assisted in setting up new devices, configuring networks, and installing required software for users and departments.",
+      "Customer Service: Delivered clear, prompt, and professional support, ensuring user satisfaction and resolving issues within established service level agreements (SLAs).",
+      "Documentation: Maintained detailed records of technical issues, solutions provided, and systems updated for future reference and audits.",
+      "With strong problem-solving skills and a customer-focused approach, I successfully contributed to the seamless operation of IT services, helping businesses achieve their technological goals.",
     ],
   },
   {
-    title: "IT Supporter And Tachnical Supporter",
-    company_name: "Bradost PC and Mobile Center",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    title: "IT Support Specialist",
+    company_name: "Sardam Center",
+    iconBg: "#0070ff",
+    date: "Jul 2019 - march 2021",
     points: [
-      "Im Worked in This center in Soran , specializes in repairing mobile phones and computers, including tasks such as formatting, data restoration,and repairing hardware components",
+      "With 2 years of hands-on experience as an IT Support Specialist, I have effectively provided technical assistance and support for various IT-related issues. My key responsibilities included:",
+
+      "Hardware and Software Troubleshooting: Diagnosed and resolved problems related to desktops, laptops, printers, and other peripherals, ensuring minimal disruption to user operations.",
+
+      "System Setup and Configuration: Assisted in the installation and configuration of new hardware and software, including operating systems and business applications, for employees and departments.",
+
+      "User Support: Provided first-level support, helping users with login issues, software errors, and connectivity problems, both remotely and on-site.",
+
+      "Network Support: Helped maintain the local network by troubleshooting connectivity issues, managing routers, and ensuring smooth internet and intranet access.",
+
+      "System Maintenance: Performed regular system checks, updates, and backups to ensure data integrity and prevent security vulnerabilities.",
+
+      "Customer Service: Delivered excellent support with a focus on clear communication, quickly addressing user concerns and ensuring a positive experience.",
+
+      "Throughout these two years, I honed my technical and communication skills, ensuring efficient resolution of issues and contributing to the seamless operation of IT services.",
     ],
   },
 ];
